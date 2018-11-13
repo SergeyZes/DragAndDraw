c40b36ec9fc6ce4d7e8c59f9b0b2d32efa802dd6
sergey <sergey_the_great@mail.ru>
2018-11-13 17:14:29 +0500

    Сделал до страницы 570.

--- app/src/main/java/com/example/zes/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/example/zes/draganddraw/BoxDrawingView.java
@@ -1,14 +1,24 @@
 package com.example.zes.draganddraw;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoxDrawingView extends View {
     private static final String TAG = "BoxDrawingView";
+
+    private Box mCurrentBox;
+    private List<Box> mBoxen = new ArrayList<>();
+    private Paint mBoxPaint;
+    private Paint mBackgroundPaint;
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -17,15 +27,23 @@ public class BoxDrawingView extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 action = "ACTION_DOWN";
+                mCurrentBox = new Box(current);
+                mBoxen.add(mCurrentBox);
                 break;
             case MotionEvent.ACTION_MOVE:
                 action = "ACTION_MOVE";
+                if (mCurrentBox != null) {
+                    mCurrentBox.setCurrent(current);
+                    invalidate();
+                }
                 break;
             case MotionEvent.ACTION_UP:
                 action = "ACTION_UP";
+                mCurrentBox = null;
                 break;
             case MotionEvent.ACTION_CANCEL:
                 action = "ACTION_CANCEL";
+                mCurrentBox = null;
                 break;
         }
         Log.i(TAG, action + " at x=" + current.x +
@@ -39,5 +57,24 @@ public class BoxDrawingView extends View {
 
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+// Фон закрашивается серовато-белым цветом
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(0xfff8efe0);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPaint(mBackgroundPaint);
+
+        for(Box box: mBoxen){
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+            canvas.drawRect(left, top, right, bottom, mBoxPaint);
+        }
+
     }
 }
